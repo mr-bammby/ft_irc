0a362facf985d7f97e52e931b04f550789084881
sbienias <sandrahanrin@gmail.com>
2022-11-22 14:51:07 +0100

    Updated the Commands enum

--- srcs/Commands.hpp
+++ srcs/Commands.hpp
@@ -1,16 +1,23 @@
+/*
+	You must be able to authenticate, set a nickname, a username, join a channel,
+		send and receive private messages using your reference client.
+	All the messages sent from one client to a channel have to be forwarded to
+		every other client that joined the channel.
+	You must have operators and regular users.
+		Then, you have to implement the commands that are specific to operators
+*/
 // https://datatracker.ietf.org/doc/html/rfc1459#section-4.1
-enum ComsInit
+enum Commands
 {
 	// Parameters: <password>
 	PASS,
 	// Parameters: <nickname> [ <hopcount> ]
 	NICK,
 	// Parameters: <username> <hostname> <servername> <realname>
-	USER
-};
-
-enum ComsMsg
-{
+	USER,
+	// Parameters: <channel>{,<channel>} [<key>{,<key>}]
+	//https://datatracker.ietf.org/doc/html/rfc1459#section-4.2.1
+	JOIN,
 	// Send private messages between users. <receiver> is the 
 	// nickname of the receiver of the message. <receiver> can also
 	// be a list of names or channels separated with commas.
@@ -21,21 +28,13 @@ enum ComsMsg
 	// replies must never be sent in response to a NOTICE message
 	// Parameters: <nickname> <text>
 	// https://datatracker.ietf.org/doc/html/rfc1459#section-4.4.2
-	NOTICE
-};
-
-enum ComsOper
-{
+	NOTICE,
+//Operator Commands
 	KICK, 				// Eject a client from the channel
 	MODE,				// Change the channel's mode
 	INVITE,				// Invite a client to an invite-only channel (mode +i)
 	TOPIC,				//Change the channel topic in a mode +t channel
-
-};
-
 // https://datatracker.ietf.org/doc/html/rfc1459#section-4.6
-enum ComsMisc
-{
 	// Available to operators and servers
 	KILL,
 	RESTART
